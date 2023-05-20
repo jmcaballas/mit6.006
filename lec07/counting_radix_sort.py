2b53273e7d2f7c6bb1df475b4068ba5d17e1e533
Joseph Matthew Caballas <joseph.caballas@bld.ai>
2023-05-20 19:41:58 +0800

    Implement Radix Sort

--- lec07/counting_radix_sort.py
+++ lec07/counting_radix_sort.py
@@ -2,24 +2,33 @@ import random
 
 
 def generate_random_array():
-    # Generate a random length between 1 and 10
-    n = random.randint(1, 10)
-    # Generate a random array with values between 1 and 10
-    return [random.randint(1, 10) for _ in range(n)]
+    # Generate a random array with 8 values between 1 and 10
+    return [random.randint(1, 1000) for _ in range(8)]
 
 
 def counting_sort(arr):
     max_value = max(arr)
-    L = [[] for _ in range(max_value + 1)]
-    for i in arr:
-        L[i].append(i)
-
+    L = [0] * (max_value + 1)
     output = []
-    for item in L:
-        output.extend(item)
+
+    for i in arr:
+        L[i] += 1
+    for j in range(len(L)):
+        output.extend([j] * L[j])
+
     return output
+
+
+def radix_sort(arr):
+    max_value = max(arr)
+    base = 1
+    while max_value // base > 0:
+        arr = counting_sort(arr)
+        base *= 10
+    return arr
 
 
 random_array = generate_random_array()
 print(random_array)
 print(counting_sort(random_array))
+print(radix_sort(random_array))
